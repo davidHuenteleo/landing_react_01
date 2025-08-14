c37709be59ad0936f93d80f84dbd7c9b94b36bb2
David Huenteleo <davidmauriciohuenteleo@gmail.com>
2025-08-14 17:25:30 -0400

    two commit

---
+++ src/components/Navbar/MobileNav/MobileNav.jsx
@@ -0,0 +1,41 @@
+import React from "react"
+import "./MobileNav.css"
+
+const MobileNav = ({ isOpen, toggleMenu }) => {
+    return(
+    <> 
+    <div
+       className={`mobile-menu ${isOpen ? "active" : ""}`}    
+       onClick={toggleMenu}>
+
+        <div className="mobile-menu-container">
+            <img className="logo" src="src/logo.svg" alt="Logo"/>   
+
+            <ul>
+                <li>
+                    <a className="menu-item">Home</a>
+                </li>
+                 <li>
+                    <a className="menu-item">Skills</a>
+                </li> 
+
+                <li>
+                    <a className="menu-item">Work Experience</a>
+                </li> 
+
+                <li>
+                    <a className="menu-item">Contact Me</a>
+                </li> 
+
+                <button className="contact-btn" onClick={() => {}}>
+                    Hire me
+                </button>
+            </ul> 
+        </div>    
+
+    </div>
+    </>
+    )
+}
+
+export default MobileNav
